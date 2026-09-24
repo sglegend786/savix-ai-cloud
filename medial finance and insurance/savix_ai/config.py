5949cdcbc2437d5a510171ae68fd32211b7ced0b
Shubham Goel <shubhamgoeltps@gmail.com>
2026-09-24 21:13:17 +0530

    Fix SQLite db path for Render deployment

--- medial finance and insurance/savix_ai/config.py
+++ medial finance and insurance/savix_ai/config.py
@@ -12,7 +12,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-key-change-me-in-production")
 
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL", "sqlite:///" + os.path.join(BASE_DIR, "instance", "savix.db")
+        "DATABASE_URL", "sqlite:///" + os.path.join(BASE_DIR, "savix.db")
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
